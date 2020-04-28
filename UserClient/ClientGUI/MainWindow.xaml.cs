0e3c47004dd2eef2da80cca724dcdbd42a5f64db
Varzk-Krethalen <varzk-krethalen@yahoo.co.uk>
2020-04-28 13:21:47 +0100

    Update + Travis YAML

--- UserClient/ClientGUI/MainWindow.xaml.cs
+++ UserClient/ClientGUI/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ClientGUI
 {
@@ -20,9 +8,21 @@ namespace ClientGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string Userdata { get; set; }
+
         public MainWindow()
         {
+            RequestLogin();
             InitializeComponent();
+            //Show();
+            textblock.Text = Userdata;
+        }
+
+        private void RequestLogin()
+        {
+            LoginWindow loginWindow = new LoginWindow { DataContext = this };
+            loginWindow.ShowDialog();
+            Userdata = loginWindow.UserData;
         }
     }
 }
