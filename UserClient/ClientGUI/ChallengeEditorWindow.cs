2e3d0c4817d0c2017e17cbc05da72d93273b17cf
Varzk-Krethalen <varzk-krethalen@yahoo.co.uk>
2020-05-29 08:08:21 +0100

    Editor Dialog cleanup

--- UserClient/ClientGUI/ChallengeEditorWindow.cs
+++ UserClient/ClientGUI/ChallengeEditorWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClientModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,11 +18,24 @@ namespace ClientGUI
     /// <summary>
     /// Interaction logic for ChallengeEditor.xaml
     /// </summary>
-    public partial class ChallengeEditorWindow : Window
+    public partial class ChallengeEditorDialog : Window
     {
-        public ChallengeEditorWindow()
+        public Challenge Challenge { get; }
+        public bool IsNewChallenge { get; } = true;
+
+        public ChallengeEditorDialog()
         {
             InitializeComponent();
+            Challenge = new Challenge();
+            DataContext = this;
+        }
+
+        public ChallengeEditorDialog(Challenge challenge)
+        {
+            InitializeComponent();
+            Challenge = challenge;
+            IsNewChallenge = false;
+            DataContext = this;
         }
 
         private void Apply_Button(object sender, RoutedEventArgs e)
